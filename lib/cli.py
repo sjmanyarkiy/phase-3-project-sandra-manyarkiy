8820ad91a00c11bbf2db3bc6f9124337687c03d1
commit-panda <sjmanyarkiy@gmail.com>
2026-01-30 01:56:31 +0300

    added main menu

---
+++ lib/cli.py
@@ -0,0 +1,11 @@
+def main_menu():
+    """Display the main menu"""
+    print("\n" + "="*50)
+    print("     FINANCE TRACKER - MAIN MENU")
+    print("="*50)
+    print("1. Manage Users")
+    print("2. Manage Expenses")
+    print("3. Manage Budgets")
+    print("4. View Reports")
+    print("5. Exit")
+    print("="*50)
