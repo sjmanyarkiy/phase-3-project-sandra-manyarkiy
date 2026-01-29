439f1e968872f486c19bfdd7c6fe28b727485201
commit-panda <sjmanyarkiy@gmail.com>
2026-01-29 18:50:47 +0300

    added name property method and setter for User class

--- lib/user.py
+++ lib/user.py
@@ -6,3 +6,19 @@ class User:
 
     def __repr__(self):
         return f'<User {self.id}: {self.name}>'
+    
+
+    @property
+    def name(self):
+        return self._name
+    
+    @name.setter
+    def name(self, name):
+        if isinstance(name, str) and len(name):
+            self._name = name
+        else:
+            raise ValueError("Name must be a string")
+        
+    def create(self):
+        sql = """CREATE TABLE IF NOT EXISTS user"""
+ 
