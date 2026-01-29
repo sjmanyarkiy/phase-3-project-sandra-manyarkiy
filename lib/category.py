9e11570c86d1286db384d1633b41a3f467a0f034
commit-panda <sjmanyarkiy@gmail.com>
2026-01-29 19:28:56 +0300

    added getter and setter method for name and user_id in category class

--- lib/category.py
+++ lib/category.py
@@ -1,4 +1,5 @@
 from __init__ import CURSOR, CONN
+from user import User
 
 class Category:
 
@@ -9,4 +10,29 @@ class Category:
         self.name = name
         self.user_id = user_id
 
+    def __repr__(self):
+        return f"<Category {self.id}: {self.name}>"
+    
+    @property
+    def name(self):
+        return self._name
+    
+    @name.setter
+    def name(self, name):
+        if isinstance(name, str) and len(name):
+            self._name = name
+        else:
+            raise ValueError("Name must be a string")
+        
+    @property
+    def user_id(self):
+        self._user_id
+
+    @user_id.setter
+    def user_id(self, user_id):
+        if isinstance(user_id, int) and User.find_by_id(user_id):
+            self._user_id = user_id
+        else:
+            raise ValueError("user_id must reference a user in the database")
+        
     
