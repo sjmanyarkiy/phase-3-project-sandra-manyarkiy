b732c47d830597e07efec2ce41ab0c74d0ccaaf8
commit-panda <sjmanyarkiy@gmail.com>
2026-01-29 18:57:35 +0300

    added save, drop and create methods for User

--- lib/user.py
+++ lib/user.py
@@ -1,3 +1,5 @@
+from __init__ import CURSOR, CONN
+
 class User:
 
     def __init__(self, name, id = None):
@@ -18,7 +20,32 @@ class User:
             self._name = name
         else:
             raise ValueError("Name must be a string")
-        
+
+    @classmethod   
     def create(self):
-        sql = """CREATE TABLE IF NOT EXISTS user"""
+        sql = """CREATE TABLE IF NOT EXISTS users (
+
+            id INTEGER PRIMARY KEY,
+            name TEXT
+        
+        )"""
+
+        CURSOR.execute(sql)
+        CONN.commit()
+
+    @classmethod
+    def drop(self):
+        sql = """DROP TABLE IF EXISTS users"""
+
+        CURSOR.execute(sql)
+        CONN.commit()
+
+    def save(self, name):
+        sql = """INSERT INTO users (name) VALUES (?)"""
+
+        CURSOR.execute(sql, (name,))
+        CONN.commit()
+
+        self.id = CURSOR.lastrowid
+        type(self).all[self.id] = self
  
